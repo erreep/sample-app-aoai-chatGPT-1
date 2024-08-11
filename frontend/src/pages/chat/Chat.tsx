e8b374818296a47ebb6c7d62f84ce72a11a8ea31
erreep <59733316+erreep@users.noreply.github.com>
2024-08-11 16:41:00 +0200

    further changes

--- frontend/src/pages/chat/Chat.tsx
+++ frontend/src/pages/chat/Chat.tsx
@@ -863,7 +863,7 @@ const Chat = () => {
                           'radial-gradient(109.81% 107.82% at 100.1% 90.19%, #0F6CBD 33.63%, #2D87C3 70.31%, #8DDDD8 100%)'
                       },
                       rootDisabled: {
-                        background: '#F0F0F0'
+                        background: '#2596be'
                       }
                     }}
                     className={styles.newChatIcon}
